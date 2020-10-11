a5fd162ec83ee183ddb70e04fb5664029220905d
fabio.maruca03@gmial.com <34383732+FabioMaruca03@users.noreply.github.com>
2020-10-11 15:26:04 +0200

    All five tests passed

--- src/main/java/comp1110/ass2/FitGame.java
+++ src/main/java/comp1110/ass2/FitGame.java
@@ -1,5 +1,7 @@
 package comp1110.ass2;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -27,9 +29,13 @@ public class FitGame {
     static boolean isPiecePlacementWellFormed(String piecePlacement) {
         if (piecePlacement.length() == 4) {
             char t = piecePlacement.toUpperCase().charAt(0);
-            if (t == 'B' || t == 'G' || t == 'Y' || t == 'R' || t == 'O' || t == 'N') {
+            if (t == 'B' || t == 'G' || t == 'Y' || t == 'R' || t == 'O' || t == 'N' || t == 'I' || t == 'L' || t == 'P' || t == 'S') {
+                if (!Character.isDigit(piecePlacement.toUpperCase().charAt(1)))
+                    return false;
                 int s = Integer.parseInt(String.valueOf(piecePlacement.toUpperCase().charAt(1)));
                 if (s >= 0 && s <= 9) {
+                    if (!Character.isDigit(piecePlacement.toUpperCase().charAt(2)))
+                        return false;
                     s = Integer.parseInt(String.valueOf(piecePlacement.toUpperCase().charAt(2)));
                     if (s >= 0 && s <= 4) {
                         t = piecePlacement.toUpperCase().charAt(3);
@@ -43,18 +49,44 @@ public class FitGame {
 
     /**
      * Determine whether a placement string is well-formed:
+     * <pre>
      * - it consists of exactly N four-character piece placements (where N = 1 .. 10);
      * - each piece placement is well-formed
      * - no shape appears more than once in the placement
      * - the pieces are ordered correctly within the string
-     *
+     * </pre>
      * @param placement A string describing a placement of one or more pieces
      * @return True if the placement is well-formed
      */
     public static boolean isPlacementWellFormed(String placement) {
         if (placement == null) return false;
         else {
-
+            char[][] pieces = new char[10][];
+            for (int i = 0, j = 0; i < placement.toCharArray().length; i+=4) {
+                // Extrapolate current piece
+                char[] temp = {placement.charAt(0), placement.charAt(1), placement.charAt(2), placement.charAt(3)};
+                if (isPiecePlacementWellFormed(String.valueOf(temp))) {
+                    for (char[] piece : pieces) {
+                        // Check repetitions
+                        if (Arrays.compare(piece, temp) == 0) {
+                            return false;
+                        }
+                    }
+                    if (j == 0)
+                        pieces[0] = temp;
+                    j++;
+                } else return false;
+            }
+            char[] keys = new char[10];
+            char[] temp;
+            for (int i = 0; i < keys.length; i++) {
+                keys[i] = placement.charAt(i*4);
+            }
+            temp = Arrays.copyOf(keys, 10);
+            Arrays.sort(keys);
+            if (Arrays.compare(keys, temp) != 0) {
+                return false;
+            }
         }
         return false; // FIXME Task 3: determine whether a placement is well-formed
     }
