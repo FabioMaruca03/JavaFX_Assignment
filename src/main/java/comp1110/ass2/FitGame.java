d83b9afcdb7fc1387cdfc82c83fcc6c94a8d3010
fabio.maruca03@gmial.com <34383732+FabioMaruca03@users.noreply.github.com>
2020-10-18 02:31:07 +0200

    Task six updates

--- src/main/java/comp1110/ass2/FitGame.java
+++ src/main/java/comp1110/ass2/FitGame.java
@@ -1,6 +1,7 @@
 package comp1110.ass2;
 
 import comp1110.ass2.model.Sizes;
+import javafx.scene.chart.PieChart;
 
 import java.util.*;
 
@@ -35,7 +36,7 @@ public class FitGame {
      * @param piecePlacement A string describing a piece placement
      * @return True if the piece placement is well-formed
      */
-    static boolean isPiecePlacementWellFormed(String piecePlacement) {
+    static synchronized boolean isPiecePlacementWellFormed(String piecePlacement) {
         if (piecePlacement.length() == 4) {
             char t = piecePlacement.toUpperCase().charAt(0); // Check for both lowercase and uppercase scenarios
             if (t == 'B' || t == 'G' || t == 'Y' || t == 'R' || t == 'O' || t == 'N' || t == 'I' || t == 'L' || t == 'P' || t == 'S') {
@@ -69,7 +70,7 @@ public class FitGame {
      * @param placement A string describing a placement of one or more pieces
      * @return True if the placement is well-formed
      */
-    public static boolean isPlacementWellFormed(String placement) {
+    public synchronized static boolean isPlacementWellFormed(String placement) {
         if (placement == null || placement.isBlank() ||
                 placement.toCharArray().length > 10*4 || placement.toCharArray().length % 4 != 0) return false;
         else {
@@ -104,59 +105,21 @@ public class FitGame {
     }
 
     private static int[][] board = new int[5][10];
-
-    /**
-     * Determine whether a placement string is valid.
-     *
-     * To be valid, the placement string must be:
-     * - well-formed, and
-     * - each piece placement must be a valid placement according to the
-     *   rules of the game:
-     *   - pieces must be entirely on the board
-     *   - pieces must not overlap each other
-     *
-     * @param placement A placement string
-     * @return True if the placement sequence is valid
-     */
-    public static boolean isPlacementValid(String placement) {
-        for (int i = 0; i < 5; i++) { // Create a blank board to emulate the game
-            Arrays.fill(board[i], 0); // 0 for free space
-        }
-        if (isPlacementWellFormed(placement)) {
-            System.out.println(placement);
+    private synchronized static boolean notOverlaps(String placement) {
+        System.out.println("soft overlap check: ");
+        printBoard();
+        System.out.println(placement);
             if (placement.isBlank()) { // if it's blank of course the board it's good
                 return true;
             } else {
-                char[][] pieces = new char[placement.toCharArray().length/4][];
-                for (int i = 0, j = 0; i < placement.toCharArray().length; i+=4, j++) {
-                    // Get all pieces
-                    pieces[j] = new char[]{
-                            placement.charAt(i), placement.charAt(i+1),
-                            placement.charAt(i+2), placement.charAt(i+3)
-                    }; // input b00E at (0, 2),.  Expected [l02W, r01W]
-                }
+                char[][] pieces = extrapolatePiecesFromPlacement(placement);
 
                 for (char[] piece : pieces) {
                     Sizes sizes = Sizes.valueOf(String.valueOf(piece[0]));
                     int xPos = Integer.parseInt(String.valueOf(piece[1]));
                     int yPos = Integer.parseInt(String.valueOf(piece[2]));
                     char orientation = piece[3];
-                    boolean d = false;
-                    switch (orientation) { // Check if it's in the board
-                        case 'N':
-                        case 'S': {
-                            if (sizes.w - 1 + xPos < 10 && sizes.h - 1 + yPos < 5)
-                                d = true;
-                            break;
-                        }
-                        case 'E':
-
-                        case 'W': {
-                            if (sizes.h - 1 + xPos < 10 && sizes.w - 1 + yPos < 5)
-                                d = true;
-                            break;
-                        }
-                    }
+                    boolean d = onBoardCheck(sizes, xPos, yPos, orientation);
 
                     if (!d) // if out of the board
                         return false;
@@ -237,19 +200,81 @@ public class FitGame {
                             break;
                         }
                     }
-                    System.out.println(); // DEBUG Print
-                    for (int j = 0; j < 5; j++) {
-                        for (int k = 0; k < 10; k++) {
-                            System.out.print(board[j][k] + "\t");
-                        }
-                        System.out.println();
-                    }
+                    printBoard();
                 }
             }
             System.out.println("Placement : "+placement+" is valid!");
             return true;
+   //     }
+   //     return false;
+    }
+
+    private static void cloneBoard(int[][] clone, int[][] board) {
+        for (int i = 0; i < board.length; i++) {
+            System.arraycopy(board[i], 0, clone[i], 0, board[i].length);
         }
-        return false;
+    }
+
+    private static void printBoard() {
+        System.out.println(); // DEBUG Print
+        for (int j = 0; j < 5; j++) {
+            for (int k = 0; k < 10; k++) {
+                System.out.print(board[j][k] + "\t");
+            }
+            System.out.println();
+        }
+    }
+
+    private static boolean onBoardCheck(Sizes sizes, int xPos, int yPos, char orientation) {
+        boolean d = false;
+        switch (orientation) { // Check if it's in the board
+            case 'N':
+            case 'S': {
+                if (sizes.w - 1 + xPos < 10 && sizes.h - 1 + yPos < 5)
+                    d = true;
+                break;
+            }
+            case 'E':
+
+            case 'W': {
+                if (sizes.h - 1 + xPos < 10 && sizes.w - 1 + yPos < 5)
+                    d = true;
+                break;
+            }
+        }
+        return d;
+    }
+
+    private static char[][] extrapolatePiecesFromPlacement(String placement) {
+        char[][] pieces = new char[placement.toCharArray().length/4][];
+        for (int i = 0, j = 0; i < placement.toCharArray().length; i+=4, j++) {
+            // Get all pieces
+            pieces[j] = new char[]{
+                    placement.charAt(i), placement.charAt(i+1),
+                    placement.charAt(i+2), placement.charAt(i+3)
+            }; // input b00E at (0, 2),.  Expected [l02W, r01W]
+        }
+        return pieces;
+    }
+
+    /**
+     * Determine whether a placement string is valid.
+     *
+     * To be valid, the placement string must be:
+     * - well-formed, and
+     * - each piece placement must be a valid placement according to the
+     *   rules of the game:
+     *   - pieces must be entirely on the board
+     *   - pieces must not overlap each other
+     *
+     * @param placement A placement string
+     * @return True if the placement sequence is valid
+     */
+    public synchronized static boolean isPlacementValid(String placement) {
+        for (int i = 0; i < 5; i++) { // Create a blank board to emulate the game
+            Arrays.fill(board[i], 0); // 0 for free space
+        }
+        return notOverlaps(placement);
     }
 
     /**
@@ -268,89 +293,171 @@ public class FitGame {
      * @param row      The location's row.
      * @return A set of all viable piece placements, or null if there are none.
      */
-    static Set<String> getViablePiecePlacements(String placement, int col, int row) {
-        Set<String> viable = null;
-        Set<String> pieces = new HashSet<>();
-        if (placement.isBlank()) { // If blank check for each possible piece variation if fits inside -> (col, row)
-            for (Sizes sizes : blankGame)
-                for (char orientation : s)
-                    if (isPlacementValid(sizes.name()+row+col+orientation) && board[col][row] == 1) {
-                        pieces.add(sizes.name()+row+col+orientation);
-                    }
-            return pieces;
-        } else if (!isPlacementValid(placement))
-            return null;
-        else for (int i = 0; i+4 < placement.length(); i+=4) {
-            String piece = placement.substring(i, i+3);
-            pieces.add(piece);
+    static synchronized Set<String> getViablePiecePlacements(String placement, int col, int row) {
+        Set<String> viable = new HashSet<>();
+        Set<Character> keys = new HashSet<>();
+        for (int i = 0; i < 5; i++) {
+            Arrays.fill(board[i], 0);
+        }
+        for (int i = 0; i < placement.length()/4; i++) {
+            keys.add(placement.charAt(i*4));
         }
 
-        for (Sizes sizes : blankGame) { // check for each possible piece variation if fits inside -> (col, row)
-            for (char orientation : s) {
-                String piece = sizes.name()+row+col+orientation;
-                if (!pieces.contains(piece)) {
-                    if (isPlacementValid(placement+piece) && board[col][row] == 1) {
-                        if (viable == null) viable = new HashSet<>();
-                        viable.add(piece);
+        char[][] pieces = extrapolatePiecesFromPlacement(placement);
+
+        for (char[] piece : pieces) {
+            Sizes sizes = Sizes.valueOf(String.valueOf(piece[0]));
+            int xPos = Integer.parseInt(String.valueOf(piece[1]));
+            int yPos = Integer.parseInt(String.valueOf(piece[2]));
+            char orientation = piece[3];
+
+            softOverlapCheck(sizes, xPos, yPos, orientation);
+            printBoard();
+        }
+
+        int[][] boardBackup = new int[5][10];
+        cloneBoard(boardBackup, board);
+
+        for (int i = 0; i < boardBackup.length; i++) {
+            int c = 0, x = -1;
+            if (i == row) {
+                if (i+1 < 4) {
+                    for (int j = 0; j < 10; j++) {
+                        if (boardBackup[i][j] == 0) {
+                            if (x == -1)
+                                x = j;
+                            c++;
+                        } else if (c < 3) {
+                            c = -1;
+                        }
+                    }
+                    if (c >= 3) {
+                        int finalI = i;
+                        int finalX = x;
+                        blankGame.forEach(it->{
+                            if (!keys.contains(Character.toLowerCase(it.name().charAt(0)))&&!keys.contains(Character.toUpperCase(it.name().charAt(0)))) {
+                                String place = it.name()+finalX+finalI+'N';
+                                if (onBoardCheck(it, finalX, finalI, 'N') && notOverlaps(place)) {
+                                    viable.add(place);
+                                }
+                                cloneBoard(board, boardBackup);
+                            }
+                        });
                     }
                 }
-                if (row < 5 && row > 0 && col < 9 && col > 0) {
-                    for (int i = -1; i < 2; i++) {
-                        piece = sizes.name()+(row-1)+(col+i)+orientation;
-                        if (!pieces.contains(piece)) {
-                            if (isPlacementValid(placement+piece) && board[col][row] == 1) {
-                                if (viable == null) viable = new HashSet<>();
-                                viable.add(piece);
-                            }
-                        }
+                cloneBoard(board, boardBackup);
+                c = 0; x = -1;
 
-                        piece = sizes.name()+(row+1)+(col+i)+orientation;
-                        if (!pieces.contains(piece)) {
-                            if (isPlacementValid(placement+piece) && board[col][row] == 1) {
-                                if (viable == null) viable = new HashSet<>();
-                                viable.add(piece);
-                            }
-                        }
-
-                        piece = sizes.name()+(row+i)+(col+1)+orientation;
-                        if (!pieces.contains(piece)) {
-                            if (isPlacementValid(placement+piece) && board[col][row] == 1) {
-                                if (viable == null) viable = new HashSet<>();
-                                viable.add(piece);
-                            }
-                        }
-
-                        piece = sizes.name()+(row+i)+(col-1)+orientation;
-                        if (!pieces.contains(piece)) {
-                            if (isPlacementValid(placement+piece) && board[col][row] == 1) {
-                                if (viable == null) viable = new HashSet<>();
-                                viable.add(piece);
-                            }
-                        }
-                        piece = sizes.name()+(row+i+1)+(col-1)+orientation;
-                        if (!pieces.contains(piece)) {
-                            if (isPlacementValid(placement+piece) && board[col][row] == 1) {
-                                if (viable == null) viable = new HashSet<>();
-                                viable.add(piece);
-                            }
-                        }
-                        piece = sizes.name()+(row-1)+(col+i+1)+orientation;
-                        if (!pieces.contains(piece)) {
-                            if (isPlacementValid(placement+piece) && board[col][row] == 1) {
-                                if (viable == null) viable = new HashSet<>();
-                                viable.add(piece);
-                            }
+                if (i-1 >= 0) {
+                    for (int j = 0; j < 10; j++) {
+                        if (boardBackup[i][j] == 0) {
+                            if (x == -1)
+                                x = j;
+                            c++;
+                        } else if (c < 3) {
+                            c = -1;
                         }
                     }
-
+                    if (c >= 3) {
+                        int finalI = i-1;
+                        int finalX = x;
+                        blankGame.forEach(it->{
+                            if (!keys.contains(Character.toLowerCase(it.name().charAt(0)))&&!keys.contains(Character.toUpperCase(it.name().charAt(0)))) {
+                                String place = it.name()+finalX+finalI+'S';
+                                if (onBoardCheck(it, finalX, finalI, 'S') && notOverlaps(place)) {
+                                    viable.add(place);
+                                }
+                                cloneBoard(board, boardBackup);
+                            }
+                        }); // todo: keep debug from test_lastP
+                    }
                 }
             }
         }
 
+
         System.out.println("Viable for : "+placement+" are "+viable);
-        return viable;
+        return viable.isEmpty()?null:viable;
         // FIXME Task 6: determine the set of all viable piece placements given existing placements
-        // removing duplicate lecters (Uppercase, Lowercase)bg  4
+    }
+
+    private static void softOverlapCheck(Sizes sizes, int xPos, int yPos, char orientation) {
+        switch (orientation) { // Overlapping cases
+            case 'N': {
+                // Pattern start
+                for (int j = 0; j < sizes.w; j++) {
+                    if (board[yPos][xPos + j] == 0) { // Check place validity
+                        board[yPos][xPos + j] = 1; // Draw
+                    }
+                }
+                if (board[yPos + 1][xPos + sizes.at[0] - 1] == 0) { // Draw first imperfection
+                    board[yPos + 1][xPos + sizes.at[0] - 1] = 1;
+                }
+
+                if (sizes.at.length == 2) {
+                    if (board[yPos + 1][xPos + sizes.at[1] - 1] == 0) { // Draw the second imperfection
+                        board[yPos + 1][xPos + sizes.at[1] - 1] = 1;
+                    }
+                }
+
+                break;
+                // Pattern end
+            }
+            case 'S': {
+                for (int j = 0; j < sizes.w; j++) {
+                    if (board[yPos + 1][xPos + j] == 0) {
+                        board[yPos + 1][xPos + j] = 1;
+                    }
+                }
+                if (board[yPos][xPos + sizes.w - sizes.at[0]] == 0) {
+                    board[yPos][xPos + sizes.w - sizes.at[0]] = 1;
+                }
+
+                if (sizes.at.length == 2) {
+                    if (board[yPos][xPos + sizes.w - sizes.at[1]] == 0) {
+                        board[yPos][xPos + sizes.w - sizes.at[1]] = 1;
+                    }
+                }
+
+                break;
+            }
+            case 'E': {
+                for (int j = 0; j < sizes.w; j++) {
+                    if (board[yPos + j][xPos + 1] == 0) {
+                        board[yPos + j][xPos + 1] = 1;
+                    }
+                }
+                if (board[yPos + sizes.at[0] - 1][xPos] == 0) {
+                    board[yPos + sizes.at[0] - 1][xPos] = 1;
+                }
+
+                if (sizes.at.length == 2) {
+                    if (board[yPos + sizes.at[1] - 1][xPos] == 0) {
+                        board[yPos + sizes.at[1] - 1][xPos] = 1;
+                    }
+                }
+
+                break;
+            }
+            case 'W': {
+                for (int j = 0; j < sizes.w; j++) {
+                    if (board[yPos + j][xPos] == 0) {
+                        board[yPos + j][xPos] = 1;
+                    }
+                }
+                if (board[yPos + sizes.w - sizes.at[0]][xPos + 1] == 0) {
+                    board[yPos + sizes.w - sizes.at[0]][xPos + 1] = 1;
+                }
+
+                if (sizes.at.length == 2) {
+                    if (board[yPos + sizes.w - sizes.at[1]][xPos + 1] == 0) {
+                        board[yPos + sizes.w - sizes.at[1]][xPos + 1] = 1;
+                    }
+                }
+
+                break;
+            }
+        }
     }
 
     /**
@@ -360,7 +467,7 @@ public class FitGame {
      * @return A placement string describing the encoding of the solution to
      * the challenge.
      */
-    public static String getSolution(String challenge) {
+    public static synchronized String getSolution(String challenge) {
         String result = "";
         List<String> pieces = new ArrayList<>();
         for (int i = 0; i < challenge.length() / 4; i++) {
